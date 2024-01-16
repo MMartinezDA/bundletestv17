4121d3b06e2c8fd21392c22952025ad89f861cf0
MMartinezDA <92915342+MMartinezDA@users.noreply.github.com>
2024-01-16 12:16:12 +0100

    Add files via upload

---
+++ pos_advanced_bundle_product/static/src/overrides/models/pos_store.js
@@ -0,0 +1,39 @@
+/** @odoo-module */
+/* Copyright (c) 2016-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>) */
+/* See LICENSE file for full copyright and licensing details. */
+/* License URL : <https://store.webkul.com/license.html/> */
+
+import { patch } from "@web/core/utils/patch";
+import { PosStore } from "@point_of_sale/app/store/pos_store";
+
+patch(PosStore.prototype, {
+    async setup() {
+        await super.setup(...arguments);
+    },
+    async _processData(loadedData) {
+        await super._processData(...arguments);
+        this._loadAdvanceBundleLines(loadedData["advance.bundle.lines"])
+        this._loadAdvanceBundleProductItems(loadedData["advance.bundle.product.items"])
+        this._loadProductProduct(loadedData['product.product']);
+        this._loadAdvanceBundleOrderLine(loadedData["advance.bundle.order.line"])
+    },
+    _loadAdvanceBundleLines(lines) {
+        var self = this;
+        self.db.bundle_lines = lines
+        self.db.bundle_lines_by_id = {};
+        self.db.bundle_lines.forEach((line)=>self.db.bundle_lines_by_id[line.id] = line)
+    },
+    _loadAdvanceBundleProductItems(lines) {
+        var self = this;
+        self.db.bundle_product_items = lines
+        self.db.bundle_product_items_by_id = {};
+        self.db.bundle_product_items.forEach((line)=>self.db.bundle_product_items_by_id[line.id] = line)
+    },
+    _loadAdvanceBundleOrderLine(lines) {
+        var self = this;
+        self.db.bundle_order_line = lines
+        self.db.bundle_orderline_by_id = {};
+        self.db.bundle_order_line.forEach((line)=>self.db.bundle_orderline_by_id[line.id] = line)
+    },
+
+})
